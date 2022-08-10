1cdaa55f61d675546f3f3b407fceb1b1d68f4984
Mert Kayhan <mert.kayhan@tum.de>
2022-08-10 11:45:39 +0200

    add stubs

--- gcs/test.py
+++ gcs/test.py
@@ -1,4 +1,11 @@
 from gcs.gcs import GCSHandler
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s,%(msecs)d | %(name)s | %(levelname)s | %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.DEBUG,
+)
 
 with GCSHandler(project="fsn-insight", bucket_name="aztec-dev", use_backoff=True) as gcs_handler:
     for blob in gcs_handler.filter_updated(gcs_handler.list_blobs(), '1985-04-12T23:20:50.52+00:00'):
--- gcs/tests/test_gcs.py
+++ gcs/tests/test_gcs.py
@@ -1,111 +1,11 @@
-import backoff
-from gcs import __version__, Bucket, Client, Blob
-from backoff import full_jitter
+from gcs import __version__
 import unittest
 
 
 class GCSTests(unittest.TestCase):
-
     def test_version(self):
         self.assertEqual(__version__, "0.1.0")
 
-    def test_backoff_params(self):
-        client = Client()
-        self.assertIsNone(client.backoff_params)
-        bucket = Bucket(client=client)
-        self.assertIsNone(bucket.backoff_params)
-        blob = Blob(name="test", bucket=bucket)
-        self.assertIsNone(blob.backoff_params)
-
-        client = Client(use_backoff=True)
-        self.assertDictEqual(client.backoff_params, {
-            "max_tries": 8,
-            "jitter": full_jitter
-        })
-        bucket = Bucket(client=client, use_backoff=True)
-        self.assertDictEqual(bucket.backoff_params, {
-            "max_tries": 8,
-            "jitter": full_jitter
-        })
-        blob = Blob(name="test", bucket=bucket, use_backoff=True)
-        self.assertDictEqual(blob.backoff_params, {
-            "max_tries": 8,
-            "jitter": full_jitter
-        })
-
-        client = Client(backoff_params={
-            "max_tries": 10,
-            "jitter": None
-        })
-        self.assertDictEqual(client.backoff_params, {
-            "max_tries": 10,
-            "jitter": None
-        })
-        bucket = Bucket(client=client, backoff_params={
-            "max_tries": 10,
-            "jitter": None
-        })
-        self.assertDictEqual(bucket.backoff_params, {
-            "max_tries": 10,
-            "jitter": None
-        })
-        blob = Blob(name="test", bucket=bucket, backoff_params={
-            "max_tries": 10,
-            "jitter": None
-        })
-        self.assertDictEqual(blob.backoff_params, {
-            "max_tries": 10,
-            "jitter": None
-        })
-
-    def test_ratelimit_params(self):
-        client = Client()
-        self.assertIsNone(client.ratelimit_params)
-        bucket = Bucket(client=client)
-        self.assertIsNone(bucket.ratelimit_params)
-        blob = Blob(name="test", bucket=bucket)
-        self.assertIsNone(blob.ratelimit_params)
-
-        client = Client(use_ratelimit=True)
-        self.assertDictEqual(client.ratelimit_params, {
-            "calls": 15,
-            "period": 900
-        })
-        bucket = Bucket(client=client, use_ratelimit=True)
-        self.assertDictEqual(bucket.ratelimit_params, {
-            "calls": 15,
-            "period": 900
-        })
-        blob = Blob(name="test", bucket=bucket, use_ratelimit=True)
-        self.assertDictEqual(blob.ratelimit_params, {
-            "calls": 15,
-            "period": 900
-        })
-
-        client = Client(ratelimit_params={
-            "calls": 1000,
-            "period": 60
-        })
-        self.assertDictEqual(client.ratelimit_params, {
-            "calls": 1000,
-            "period": 60
-        })
-        bucket = Bucket(client=client, ratelimit_params={
-            "calls": 1000,
-            "period": 60
-        })
-        self.assertDictEqual(bucket.ratelimit_params, {
-            "calls": 1000,
-            "period": 60
-        })
-        blob = Blob(name="test", bucket=bucket, ratelimit_params={
-            "calls": 1000,
-            "period": 60
-        })
-        self.assertDictEqual(blob.ratelimit_params, {
-            "calls": 1000,
-            "period": 60
-        })
 
 if __name__ == "__main__":
     unittest.main()
